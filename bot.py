e6c0de99d6ed2791b511f5442d81975f771ff71a
Kent Coble <kamakazikamikaze@gmail.com>
2020-01-25 20:29:15 -0600

    Updated player_info function

--- bot.py
+++ bot.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
+from bs4 import BeautifulSoup
+from itertools import zip_longest
+from json import loads
 from json.decoder import JSONDecodeError
 import logging
 import logging.handlers
+from operator import itemgetter
 from praw import Reddit
-import requests
+from re import findall
+from requests import get
 from sys import argv
+from urllib.parse import urlsplit, parse_qs
 
 
 def setup_logging():
@@ -35,8 +41,8 @@ def bot_help(contents):
 (Note: Replace PLAT with a platform from above)
 
 * help
-* player PLAT {{summary, efficiency, activity}} NAME
-* player PLAT tanks {{efficiency, top, recent}} NAME
+* player PLAT {{summary, recent, efficiency}} NAME
+* player PLAT tanks {{efficiency, top}} NAME
 * clan {{summary, active, battles, players, tier, top}} NAME
 * community PLAT {{summary, today}}
 * community PLAT {{active, inactive, new}} DAYS
@@ -45,12 +51,303 @@ def bot_help(contents):
 # Example Usage
 
 `/u/{0} clan summary RDDT`
+
 `/u/{0} player xbox summary DEZERTstorm03`""".format(
         contents[0].split('/')[-1])
 
 
+def grouper(n, iterable, fillvalue=None):
+    r"""
+    Step over an object, returning n-sized chunks
+
+    :param int n: Size of chunks to return
+    :param iter iterable: iterable object to step over
+    :param fillvalue: Values to use if iterable is not divisible by n
+    """
+    args = [iter(iterable)] * n
+    return zip_longest(fillvalue=fillvalue, *args)
+
+
 def player_info(contents):
-    pass
+    # TODO: Finish all valid commands
+    PLAYER_VALID = {
+        'summary': (
+            '##Name: {0[Name]}\n\n'
+            '|Stat|Career Average|\n'
+            ':--|:--\n'
+            'Eff|{0[Eff]}\n'
+            'WN7|{0[WN7]}\n'
+            'WN8|{0[WN8]}\n\n'
+        ),
+        'recent': (
+            '##Name: {0[Name]}\n\n'
+            '|Stat|Week|Month|Overall|\n'
+            ':--|:--|:--|:--\n'
+        ),
+        'efficiency': (
+            '##Name: {0[Name]}\n\n'
+            '|From|To|Efficiency|WN7|WN8|Battles|Wins|\n'
+            ':--|:--|:--|:--|:--|:--|:--\n'
+        )
+    }
+    PLAYER_TANK_VALID = {
+        'efficiency': (
+            '##Name: {0[Name]}\n\n'
+            '|Tier|Mastery|Name|Type|Win rate|Wins|Battles|Avg Dmg|'
+            'Percent of all battles|Efficiency|WN8|\n'
+            ':--|:--|:--|:--|:--|:--|:--|:--|:--|:--|:--\n'
+        ),
+        'top': (
+            '##Name: {0[Name]}\n\n'
+            '*Note: These tanks are sorted by WN8 and Total battles. This '
+            'sorting is arbitrary and may not accurately reflect the '
+            'overall performance of the player*\n\n'
+            '|Tier|Mastery|Name|Type|Win rate|Wins|Battles|Avg Dmg|'
+            'Percent of all battles|Efficiency|WN8|\n'
+            ':--|:--|:--|:--|:--|:--|:--|:--|:--|:--|:--\n'
+        )
+    }
+    if contents[2].lower() not in ('ps4', 'xbox'):
+        return 'Bad platform. Please use "ps4" or "xbox" and try again!'
+    contents[3] = contents[3].lower()
+    contents[4] = contents[4].lower()
+    if contents[3] == 'tanks' and len(contents) < 5:
+        return 'Something is wrong with your request. Please retry!'
+    elif contents[3] == 'tanks' and contents[4] not in PLAYER_TANK_VALID:
+        return 'Bad subcommand request "{}". Please retry!'.format(contents[4])
+    elif len(contents) < 4:
+        return 'Something is wrong with your request. Please retry!'
+    elif contents[3] != 'tanks' and contents[3] not in PLAYER_VALID:
+        return 'Bad subcommand request "{}". Please retry!'.format(contents[3])
+    offset = 5 if contents[3] == 'tanks' else 4
+    r = get(
+        'http://www.wotinfo.net/en/efficiency',
+        params={
+            'server': contents[2],
+            'playername': ' '.join(
+                contents[offset:])
+        })
+    if r.status_code != 200:
+        return (
+            'I received an error code of {} from the server. Please try again '
+            'later!'
+        )
+    if contents[3] == 'tanks':
+        player = {'Name': ' '.join(contents[offset:])}
+        soup = BeautifulSoup(r.content)
+        href = soup.find('li', class_='activemenu').a['href']
+        try:
+            playerid = parse_qs(urlsplit(href).query)['playerid'][0]
+        except KeyError:
+            return (
+                'Sorry, it looks like you either have an invalid player '
+                'name or the wrong platform in your request. Please '
+                'review your query and try again. If you are still having '
+                'issues, please contact my author for assistance!'
+            )
+        vehicles = get(
+            'http://wotinfo.net/en/vehicles',
+            params={
+                'playerid': playerid,
+                'server': contents[2]})
+        if vehicles.status_code != 200:
+            return (
+                'I was able to find this user on wotinfo but I '
+                'experienced an error accessing their vehicle statistics. '
+                'Tagging /u/KamikazeRusher to review this when he gets '
+                'the mention notification.'
+            )
+        # vehicles_soup = BeautifulSoup(vehicles.content)
+        # source = vehicles_soup.find(
+        #     'script',
+        #     src='https://www.google.com/jsapi'
+        # ).next_sibling.next_sibling
+        ranks = {
+            'rank_01.png': '1',
+            'rank_02.png': '2',
+            'rank_03.png': '3',
+            'rank_m.png': 'M',
+            'FFFFFF-0.png': ''
+        }
+        # Extract all Google Visualization rows that get added
+        rows = []
+        for row in findall(
+                r'data\.addRow\([^\;]+;', vehicles.content.decode('utf8')):
+            # We're cheating here by taking the data entered into the rows and
+            # forcing it into a JSON format for easy parsing.
+            data = []
+            r = loads('{ "data": ' + row[12:-2].replace(
+                # Replace the double-quotes with a character not used
+                '"', '!').replace(
+                # Change the single-quotes to double-quotes
+                "'", '"').replace(
+                # Change the original double-quotes to single-quotes
+                '!', "'").replace(
+                # Surround parameters with quotes for JSON validity
+                'v:', '"v":').replace(
+                'f:', '"f":') + '}')['data']
+            # Tier
+            data.append(r[0])
+            # Mastery
+            data.append(ranks[r[1].split("'")[1].split('/')[-1]])
+            # Name
+            data.append(r[3].split('>')[1][:-3])
+            # Type
+            data.append(r[5].split("'")[-2].split('/')[-1].split('.')[0])
+            # Win rate
+            data.append(r[6]['f'])
+            # Wins
+            data.append(r[7])
+            # Battles
+            data.append(r[8])
+            # Average damage
+            data.append(r[9])
+            # % of all battles
+            data.append(r[10]['f'])
+            # Efficiency
+            data.append(r[11]['v'])
+            # WN8
+            data.append(r[12]['v'])
+            rows.append(data)
+        if contents[4] == 'efficiency':
+            return PLAYER_TANK_VALID[
+                contents[offset - 1]
+            ].format(player) + '\n'.join(
+                map(
+                    lambda r: '{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}'.format(*r),
+                    rows)
+            ) + '\n\nSource: {}\n'.format(vehicles.url)
+        elif contents[4] == 'top':
+            sorted_rows = sorted(rows, key=itemgetter(6, 10), reverse=True)
+            return PLAYER_TANK_VALID[
+                contents[offset - 1]
+            ].format(player) + '\n'.join(
+                map(
+                    lambda r: '{}|{}|{}|{}|{}|{}|{}|{}|{}|{}|{}'.format(*r),
+                    sorted_rows[:10])
+            ) + '\n\nSource: {}\n'.format(vehicles.url)
+    else:
+        player = {'Name': ' '.join(contents[offset:])}
+        soup = BeautifulSoup(r.content)
+        if contents[3] == 'summary':
+            var = soup.find_all('var')
+            if len(var) != 6:
+                return (
+                    'I did not receive an expected response, which means '
+                    'you may have entered the wrong player name or '
+                    'platform by accident. Please review your request and '
+                    'try again!'
+                )
+            else:
+                for name, value in grouper(2, var):
+                    player[name.text.strip()] = value.text.strip()
+                return PLAYER_VALID[
+                    contents[offset - 1]
+                ].format(player) + '\nSource: {}'.format(r.url)
+        elif contents[3] == 'recent':
+            href = soup.find('li', class_='activemenu').a['href']
+            try:
+                playerid = parse_qs(urlsplit(href).query)['playerid'][0]
+            except KeyError:
+                return (
+                    'Sorry, it looks like you either have an invalid player '
+                    'name or the wrong platform in your request. Please '
+                    'review your query and try again. If you are still having '
+                    'issues, please contact my author for assistance!'
+                )
+            recent = get(
+                'http://wotinfo.net/en/recent',
+                params={
+                    'playerid': playerid,
+                    'server': contents[2]})
+            if r.status_code != 200:
+                return (
+                    'I was able to find this user on wotinfo but I '
+                    'experienced an error accessing their recent statistics. '
+                    'Tagging /u/KamikazeRusher to review this when he gets '
+                    'the mention notification.'
+                )
+            recent_soup = BeautifulSoup(recent.content)
+            # table = recent_soup.find('div', class_='container col-sm-12')
+            stats = {'week': [], 'month': [], 'overall': []}
+            week_and_month = recent_soup.find_all(
+                'div', class_='col-xs-4 col-sm-4 my_plan1')
+            overall = recent_soup.find_all(
+                'div', class_='col-xs-4 col-sm-4 my_plan2')
+            titles = recent_soup.find_all(
+                'div', class_='col-xs-12 col-sm-3 my_feature')
+            stats['titles'] = [[i.strip() for i in s.strings][0]
+                               for s in titles]
+            i = 0
+            # for period, title in zip_longest(week_and_month, titles):
+            for period in week_and_month:
+                if i % 2 == 0:
+                    stats['week'].append(period.text.split()[0])
+                else:
+                    stats['month'].append(period.text.split()[0])
+                i += 1
+            # for period, title in zip(overall, titles):
+            for period in overall:
+                stats['overall'].append(period.text.split()[0])
+            return PLAYER_VALID[
+                contents[offset - 1]
+            ].format(player) + '\n'.join(
+                map(
+                    lambda z: '{}|{}|{}|{}'.format(*z),
+                    zip(
+                        stats['titles'],
+                        stats['week'],
+                        stats['month'],
+                        stats['overall']))
+            ) + '\n\nSource: {}\n'.format(recent.url)
+        elif contents[3] == 'efficiency':
+            href = soup.find('li', class_='activemenu').a['href']
+            try:
+                playerid = parse_qs(urlsplit(href).query)['playerid'][0]
+            except KeyError:
+                return (
+                    'Sorry, it looks like you either have an invalid player '
+                    'name or the wrong platform in your request. Please '
+                    'review your query and try again. If you are still having '
+                    'issues, please contact my author for assistance!'
+                )
+            trend = get(
+                'http://wotinfo.net/en/trend',
+                params={
+                    'playerid': playerid,
+                    'server': contents[2]})
+            if trend.status_code != 200:
+                return (
+                    'I was able to find this user on wotinfo but I '
+                    'experienced an error accessing their efficiency trend. '
+                    'Tagging /u/KamikazeRusher to review this when he gets '
+                    'the mention notification.'
+                )
+            trend_soup = BeautifulSoup(trend.content)
+            # data_parent = trend_soup.find('ul', class_='thumbnails')
+            # data = data_parent.find_all('div', class_='row')
+            data = trend_soup.find_all('ul', class_='event-list')
+            all_data = []
+            for point in data:
+                l = []
+                # times = point.li('time')
+                # l.append(' '.join(times[0].text.split()[1:]))
+                # l.append(' '.join(times[1].text.split()[1:]))
+                for time in point.li('time'):
+                    l.append(' '.join(time.text.split()[1:]))
+                for div in point.find_all('div', class_='progress'):
+                    l.append(div.text.strip())
+                i = 0
+                for li in point.li.div.ul.find_all('li'):
+                    if i < 2:
+                        l.append(li.text.split()[0])
+                    i += 1
+                all_data.append(l)
+            return PLAYER_VALID[contents[
+                offset - 1]
+            ].format(player) + '\n'.join(
+                map(lambda l: '{}|{}|{}|{}|{}|{}|{}'.format(*l), all_data)
+            ) + '\n\nSource: {}\n'.format(trend.url)
 
 
 def clan_info(contents):
@@ -115,13 +412,14 @@ def clan_info(contents):
     }
     if len(contents) < 4:
         return 'No clan name entered. Please retry!'
-    if contents[2].lower() in CLAN_VALID:
-        r = requests.get('https://wotclans.com.br/api/clan/' + contents[3])
+    contents[2] = contents[2].lower()
+    if contents[2] in CLAN_VALID:
+        r = get('https://wotclans.com.br/api/clan/' + contents[3])
         if r.status_code == 200:
             try:
                 data = r.json()
                 players = []
-                if contents[2].lower() == 'active':
+                if contents[2] == 'active':
                     players = [
                         '{0[Name]} | {0[MonthBattles]}'.format(
                             p) for p in list(
@@ -130,14 +428,14 @@ def clan_info(contents):
                                 data['Players']))]
                     players.insert(0, 'Player | Months\'s battles')
                     players.insert(1, ':-:|:-:')
-                elif contents[2].lower() == 'players':
+                elif contents[2] == 'players':
                     players = [
                         '{0[Name]} | {0[TotalWn8]} | {0[MonthWn8]}'.format(
                             p) for p in data['Players']]
                     players.insert(
                         0, 'Player | Total WN8 | Month\'s WN8')
                     players.insert(1, ':-:|:-:|:-:')
-                elif contents[2].lower() == 'tiers':
+                elif contents[2] == 'tiers':
                     players = [
                         '{0[Name]} | {0[TotalTier]} | {0[MonthTier]}'.format(
                             p) for p in data['Players']]
@@ -148,7 +446,7 @@ def clan_info(contents):
                             'Month\'s tier average'
                         ))
                     players.insert(1, ':-:|:-:|:-:')
-                elif contents[2].lower() == 'top':
+                elif contents[2] == 'top':
                     top_all = sorted(
                         data['Players'],
                         key=lambda p: p['TotalWn8'],
@@ -196,7 +494,8 @@ def thank_you(contents):
     return """Thank you! I may not be handsome, but I hope you at least find me
 handy!
 
-Credit for my development goes to /u/KamikazeRusher. Credit for data goes to
+Credit for my development goes to
+[KamikazeRusher](https://reddit.com/u/KamikazeRusher). Credit for data goes to
 the author of the cited source(s)."""
 
 
@@ -231,7 +530,7 @@ def tank_info(contents):
     if len(contents) < 4:
         return 'No tank name entered. Please retry!'
     if contents[2].lower() in TANK_VALID:
-        r = requests.get(
+        r = get(
             'https://wotclans.com.br/api/tanks/{}'.format(
                 contents[2].lower()),
             params={'tank': ' '.join(contents[3:])}
@@ -277,6 +576,8 @@ VALID = {
 
 def parse(message):
     contents = message.body.split()
+    if len(contents) == 1:
+        return VALID['help'](contents)
     if contents[1].lower() not in VALID:
         return """Oops! Your first command is not valid. Please review your
 spelling and try again. If you continue to have this issue, please
@@ -286,8 +587,21 @@ check the wiki over at /r/{} or ask a mod there for help.""".format(
         return VALID[contents[1].lower()](contents)
 
 
-def process(message):
-    message.reply(parse(message))
+def process(message, reddit):
+    response = parse(message)
+    if len(response) <= 10000:
+        message.reply(response)
+    else:
+        sub = reddit.subreddit(reddit.config.username)
+        submission = sub.submit(
+            'Response to ' + str(message.author),
+            response,
+            send_replies=False)
+        message.reply((
+            "I'm sorry, it appears my response is greater than the maximum "
+            "allowed comment length. (Max: 10000, Mine: {}). I have created a "
+            "self-post at {}"
+        ).format(len(response), submission.url))
 
 
 def run(bot_name, subreddits):
@@ -301,7 +615,7 @@ def run(bot_name, subreddits):
             # We don't respond to direct messages
             message.mark_read()
         elif message.subreddit.display_name.lower() in subreddits:
-            process(message)
+            process(message, reddit)
             message.mark_read()
             logger.debug('Completed message {0.id}'.format(message))
         else:
